63230cec8b460aa5cc179d7aca3586915fb633e6
Said Mounaim <saidmounaim00@gmail.com>
2023-12-31 00:26:39 +0100

    Add Register Page

--- app/(root)/(auth)/register/page.tsx
+++ app/(root)/(auth)/register/page.tsx
@@ -3,14 +3,25 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
 
-const LoginPage = () => {
+const RegisterPage = () => {
   return (
     <section className="h-[85vh] flex justify-center items-center">
       <div className="w-[350px] sm:max-w-full bg-[#11235A] flex flex-col rounded-lg items-center p-4">
-        <h1 className="text-2xl font-bold text-white">Login</h1>
+        <h1 className="text-2xl font-bold text-white">Register</h1>
 
         <div className="w-full flex flex-col mt-4">
           <div className="grid w-full items-center gap-y-2">
+            <Label htmlFor="fullname" className="text-white text-md">
+              Full Name:
+            </Label>
+            <Input
+              type="text"
+              id="fullname"
+              placeholder="Full Name"
+              className="w-full"
+            />
+          </div>
+          <div className="grid w-full items-center gap-y-2 mt-3">
             <Label htmlFor="email" className="text-white text-md">
               Email Address:
             </Label>
@@ -34,14 +45,14 @@ const LoginPage = () => {
           </div>
           <div className="w-full mt-4">
             <Button className="w-full bg-[#7ED7C1] text-white text-md transition hover:bg-[#7ED7C1]/80">
-              Login
+              Regsiter
             </Button>
           </div>
           <div className="w-full mt-5">
             <p className="text-white text-md text-center">
-              Don't have an account?{" "}
-              <Link href="/register" className="text-[#7ED7C1]">
-                Register
+              Already have an account?{" "}
+              <Link href="/login" className="text-[#7ED7C1]">
+                Login
               </Link>
             </p>
           </div>
@@ -51,4 +62,4 @@ const LoginPage = () => {
   );
 };
 
-export default LoginPage;
+export default RegisterPage;
